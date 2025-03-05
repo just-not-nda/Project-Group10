e5350c5edbe4f5ba8d484f25b975946853cf5ef4
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-03-05 19:51:08 +0700

    add all

--- src/Project_Group10.cpp
+++ src/Project_Group10.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 using namespace std;
 
-const int nm = 101;
-int i, n, cnt, queen[nm][nm];
-bool col[nm], dia1[nm], dia2[nm];
+const int nm = 20;
+int N, cnt, queen[nm];
+bool col[nm], dia1[2 * nm], dia2[2 * nm];
 
 void Print_Result();
-bool Can_Place_Queen(int, int, const int);
+bool Can_Place_Queen(int, int);
 void Place_Queen(int);
 
 int main()
 {
-    cin >> n;
-    for(int i = 1; i <= 2 * n; i++)
-    {
+    cin >> N;
+    for (int i = 1; i <= N; i++)
         col[i] = true;
+    for (int i = 1; i <= 2 * N; i++)
+    {
         dia1[i] = true;
         dia2[i] = true;
     }
@@ -27,37 +28,36 @@ void Print_Result()
 {
     cnt++;
     cout << "Way " << cnt << ":" << endl;
-    for(int i = 1; i <= n; i++)
+    for(int i = 1; i <= N; i++)
     {
-        for(int j = 1; j <= n; j++)
-            cout << queen[i][j] << " ";
+        for(int j = 1; j <= N; j++)
+            if(queen[i] == j)
+                cout << i << " ";
+            else
+                cout << "0 ";
         cout << endl;
     }
     cout << endl;
 }
 
-bool Can_Place_Queen(int i, int j, const int n)
+bool Can_Place_Queen(int i, int j)
 {
-    if(col[j] == true && dia1[i - j + n] == true && dia2[i + j - 1] == true)
-        return true;
-    return false;
+    return col[j] && dia1[i - j + N] && dia2[i + j - 1];
 }
 void Place_Queen(int i)
 {
-    for(int j = 1; j <= n; j++)
+    for(int j = 1; j <= N; j++)
     {
-        if(Can_Place_Queen(i, j, n))
+        if(Can_Place_Queen(i, j))
         {
-            queen[i][j] = i;
-            col[j] = false;
-            dia1[i - j + n] = false;
-            dia2[i + j - 1] = false;
-            if(i == n)Print_Result();
-            else Place_Queen(i + 1);
-            queen[i][j] = 0;
-            col[j] = true;
-            dia1[i - j + n] = true;
-            dia2[i + j - 1] = true;
+            queen[j] = i;
+            col[j] = dia1[i - j + N] = dia2[i + j - 1] = false;
+            if(i == N)
+                Print_Result();
+            else
+                Place_Queen(i + 1);
+
+            col[j] = dia1[i - j + N] = dia2[i + j - 1] = true;
         }
     }
 }
